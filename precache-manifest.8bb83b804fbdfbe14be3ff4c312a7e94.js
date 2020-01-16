c14ad7da8a5e7b4a67f6665d32d767e7684044f7
Piotr Kowalczyk <p.kowalczyk.95@wp.pl>
2020-01-16 21:37:48 +0100

    Updates

---
+++ precache-manifest.8bb83b804fbdfbe14be3ff4c312a7e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96eaad9cb667baa1de9d4bf3f1f658f",
+    "url": "/weatherReactApp/index.html"
+  },
+  {
+    "revision": "ee92a210a739bde29725",
+    "url": "/weatherReactApp/static/css/main.35dc9171.chunk.css"
+  },
+  {
+    "revision": "7edee0c10c2813784ad1",
+    "url": "/weatherReactApp/static/js/2.b587ef71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherReactApp/static/js/2.b587ef71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee92a210a739bde29725",
+    "url": "/weatherReactApp/static/js/main.ffc453bc.chunk.js"
+  },
+  {
+    "revision": "b69b9104d516e91de63a",
+    "url": "/weatherReactApp/static/js/runtime-main.3fc2e1ab.js"
+  }
+]);
